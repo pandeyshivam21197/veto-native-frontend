2b2fe911742ebe116b0b990e24e7074ec6d74d74
Shivam <shivam.pandey@nineleaps.com>
2020-04-30 13:57:41 +0530

    code refactored

--- src/components/molecules/FormSubmitButton.tsx
+++ src/components/molecules/FormSubmitButton.tsx
@@ -4,26 +4,31 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 
 interface IFormButton {
-    formProps: FormikProps<FormikValues>;
-    buttonTitle: string;
+  formProps: FormikProps<FormikValues>;
+  buttonTitle: string;
 }
 
 const FormSubmitButton = (props: IFormButton) => {
-    const {buttonTitle, formProps} = props;
-    const {handleSubmit} = formProps;
+  const {buttonTitle, formProps} = props;
+  const {handleSubmit} = formProps;
 
-    return (
-        <Button onPress={handleSubmit} title={buttonTitle} buttonType={'ghost'} containerStyle={styles.container} isLoading={false}/>
-    )
-}
+  return (
+    <Button
+      onPress={handleSubmit}
+      title={buttonTitle}
+      buttonType={'ghost'}
+      containerStyle={styles.container}
+      isLoading={false}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 60,
-        width: 150,
-        alignSelf: 'center',
-        backgroundColor: 'red'
-    },
+  container: {
+    marginTop: 60,
+    width: 150,
+    alignSelf: 'center',
+  },
 });
 
 export default FormSubmitButton;
